5f41d10ee88f985ce139605c337a23b08838d14e
Abner Rosa (Zi) <abnerdsr@gmail.com>
2024-09-25 23:50:10 -0300

    add Laravel Octane and api routes

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return <<<HTML
+        <div>
+            <h1>Olá Família.</h1>
+            <p>
+                Em breve um novo pedacinho da internet dedicado a maior lenda gamer irá
+                aparecer por aqui.
+            </p>
+        </div>
+    HTML;
 });
 
 require __DIR__.'/auth.php';
